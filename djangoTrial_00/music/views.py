2d6a64a77242591ae13baffaa349cdd2c47f0a0f
albertusk95 <albertusk15@gmail.com>
2016-09-06 21:39:15 +0700

    connecting to db

--- djangoTrial_00/music/views.py
+++ djangoTrial_00/music/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-	return HttpResponse("<h1>This is my Music app homepage</h1>")
+	all_albums = Album.objects.all()
+	html = ''
+	for album in all_albums:
+		url = '/music/' + str(album.id) + '/'
+		html += '<a href="' + url + '">' + album.album_title + '</a><br />'
+	
+	return HttpResponse(html)
+	
+def detail(request, album_id):
+        return HttpResponse("<h2>Album ID: " + str(album_id) + "</h2>")
