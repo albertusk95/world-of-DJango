d535a81d2ead3f352f736da6c8541f3066aa53db
albertusk95 <albertusk15@gmail.com>
2016-09-08 00:18:53 +0700

    added generic view

--- djangoTrial_00/music/urls.py
+++ djangoTrial_00/music/urls.py
@@ -5,10 +5,10 @@ app_name = "music"
 
 urlpatterns = [
     # music/
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
 
     # music/id/
-    url(r'^(?P<albumIDs>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 	
 	# music/id/favorite
     url(r'^(?P<albumIDs>[0-9]+)/favorite/$', views.favorite, name="favorite")
--- djangoTrial_00/music/views.py
+++ djangoTrial_00/music/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
 
+from django.views import generic
+
+# Generic view - classes
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	
+	def get_queryset(self):
+		return Album.objects.all()
+	
+
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
+	
+
+# Normal view - functions
 def index(request):
 	all_albums = Album.objects.all()
 	return render(request, 'music/index.html', {'all_albums': all_albums})
+
 	
 def detail(request, albumIDs):
 	album = get_object_or_404(Album, pk=albumIDs)
 	return render(request, 'music/detail.html', {'album': album})
+
 	
 def favorite(request, albumIDs):
 	album = get_object_or_404(Album, pk=albumIDs)
